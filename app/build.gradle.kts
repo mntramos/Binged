c1a9bcc76f9e18dec20bb6d8108afc49bdb4521d
Mark <mntramos8@gmail.com>
2025-03-18 01:19:55 +0800

    setup Koin for app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Koin dependencies
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
 }
